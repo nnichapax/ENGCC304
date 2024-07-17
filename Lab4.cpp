1fd7b2a314eb12195cc37f3938793cf9679dff8c
MOOMIN <nichapha8895@gmail.com>
2024-07-17 14:38:52 +0700

    Create Lab4.cpp

---
+++ Lab4.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+
+    char ID [ 10 ] ;
+    int WorkingHrs ;
+    float SalaryAmountHr ;
+    float SUM ;
+
+    printf( "Input the Employees ID : " ) ;
+    scanf( "%s", ID ) ;
+
+    printf( "Input the working hrs : " ) ;
+    scanf( "%d", &WorkingHrs ) ;
+
+    printf( "Salary amount/hr : " ) ;
+    scanf( "%f", &SalaryAmountHr ) ;
+
+    SUM = WorkingHrs * SalaryAmountHr ;
+
+    printf( "\n Employees ID = %s", ID ) ;
+    printf( "\n Salary = U$ %.2f", SUM ) ;
+
+    return 0 ;
+
+}//end function
