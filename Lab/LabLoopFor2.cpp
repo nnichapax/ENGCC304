7f526e65c34edf47530432971cbe6c7e8137c7f4
nnichapax <nichapha8895@gmail.com>
2024-08-22 20:16:29 +0700

    update

--- Lab/LabLoopFor2.cpp
+++ Lab/LabLoopFor2.cpp
@@ -3,9 +3,9 @@ int main() {
     int number = 0 ;
     printf( "enter your number : " ) ;
     scanf( "%d", &number ) ;
-    if( number % 2 == 0 ) {
-        for( int i = 0 ; i < number ; i++ ) {
-            for( int j = 0 ; j < number ; j++ ) {
+    if( number % 2 == 0 ){
+        for( int i = 0 ; i < number ; i++ ){
+            for( int j = 0 ; j < number ; j++ ){
                 if( j == i ){
                     printf( "1" ) ;
                 } else{
@@ -14,10 +14,10 @@ int main() {
             }//end for
             printf( "\n" ) ;
         }//end for
-    } else if( number % 2 == 1 ) {
-        for( int i = number ; i != 0 ; i-- ) {
-            for( int j = 1 ; j <= number ; j++ ) {
-                if( j == i ) {
+    } else if( number % 2 == 1 ){
+        for( int i = number ; i != 0 ; i-- ){
+            for( int j = 1 ; j <= number ; j++ ){
+                if( j == i ){
                     printf( "1" ) ;
                 } else{
                    printf( "0" ) ;    
