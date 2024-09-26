06c5e6d69b89c58a648a836de46bd69478c79b19
nnichapax <nichapha8895@gmail.com>
2024-09-26 23:21:19 +0700

    update

---
+++ Lab/Lab10.cpp
@@ -0,0 +1,57 @@
+/*
+    จงเขียนโปรแกรมเพื่อรับคำจากผู้ใช้งาน เพื่อตรวจสอบว่า คำที่กรอกมามีลักษณะเป็นคำหรือวลีที่สามารถอ่านจากหลังไปหน้าหรือหน้าไปหลังแล้วยังคงความหมายเหมือนเดิมได้
+    โดยที่ หากคำนั้นสามารถอ่านจากหน้าไปหลังหรือหลังไปได้ ให้แสดงผลลัพธ์ว่า Pass แต่หากทำไม่ได้ให้ขึ้นว่า Not Pass
+
+    Test case:
+        Enter word:
+            radar
+    Output:
+        Pass.
+
+    Test case:
+        Enter word:
+            hello
+    Output:
+        Not Pass.
+
+    Test case:
+        Enter word:
+            Radar
+    Output:
+        Pass.
+
+    Test case:
+        Enter word:
+            here
+    Output:
+        Not Pass.
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>//ใช้นำเข้าหัวข้อไฟล์ (header file) ที่ใช้ในการจัดการกับตัวอักษร
+
+int main() {
+    char word [50] ;
+    int lenght, i, palindrome = 1 ;
+
+    printf( "Enter word: \n" ) ;
+    scanf( "%s", word ) ;
+
+    lenght = strlen( word ) ;//lenght คือ ตัวความยาวของคำหรือวลี
+
+    for( i = 0 ; i < lenght / 2 ; i++ ) {//ที่หาร2เพราะว่าตัวตรงกลางเป็นตัวเดิมอยู่แล้ว
+        if( tolower( word[i] ) != tolower( word[lenght - i - 1] ) ) {//tolower คือ แปลงตัวอักษรพิมพ์ใหญ่เป็นพิมพ์เล็ก
+        palindrome = 0 ;//palindrome คือ กลุ่มคำหรือประโยคที่สะกดเหมือนดเิมทั้งจากหน้าไปหลัง
+        break ;
+        }
+    }
+
+    if( palindrome ) {//ถ้าเป็นpalindrome เข้าอันนี้
+        printf( "Pass.\n" ) ;
+    } else {//อันนี้คือไม่เป็น palindrome
+        printf( "Not Pass.\n" ) ;
+    }
+
+    return 0 ;
+}//end main function
---
+++ Lab/Lab11.cpp
@@ -0,0 +1,69 @@
+/*
+    จงเขียนฟังก์ชันในการตรวจสอบตัวเลขที่ผู้ใช้ป้อนเข้ามา ว่าเป็นตัวเลขที่เป็นอาร์มสตรองหรือไม่ หากใช่ให้แสดงคำว่า Pass หากไม่ใช่แสดงคำว่า Not Pass
+    
+    หมายเหตุ: ตัวเลขอาร์มสตรอง คือ ตัวเลขที่ผลรวมของเลขยกกำลังของจำนวนหลัก เช่น
+    153 เป็นตัวเลขอาร์มสตรอง เพราะ 153 มีทั้งหมด 3 หลัก จึงมีค่าเท่ากับ = 1^3 +  5^3 +  3^3 (โดยที่เครื่องหมาย ^ หมายถึงเลขยกกำลัง)
+    370 = 3^3 + 7^3 + 0^3
+
+    Test Case:
+        Enter Number:
+            153
+    Output:
+        Pass.
+    
+    Test Case:
+        Enter Number:
+            370
+    Output:
+        Pass.
+    
+    Test Case:
+        Enter Number:
+            372
+    Output:
+        Not Pass.
+*/
+
+#include <stdio.h>
+#include <math.h>//Mathematics Library
+
+int Armstrong( int number ) {//function
+    int orinumber, remain, result = 0, n = 0 ;//ค่าตัวเลขเดิม, เก็บหลักสุดท้ายของตัวเลข, ผลรวมเลขยกกำลัง, จำนวนหลักของตัวเลข
+
+    orinumber = number ;//เก็บค่าตัวเลข
+
+    while ( orinumber != 0 ) {
+        orinumber /= 10 ;//แบ่งตัวเลขเป็น 10 เพื่อเอาเลขหลักน้อยกว่า
+        ++n ;//บวกหลักตัวเลข
+    }
+
+    orinumber = number ;//คืนค่าตัวเลข
+
+    while ( orinumber != 0 ) {
+        remain = orinumber % 10 ;//คำนวณหาเลขหลักสุดท้าย
+        result += pow( remain, n ) ;//ยกกำลังและเพิ่มresult
+        orinumber /= 10 ;//แบ่งตัวเลขเพื่อลบเลขหลักสุดท้ายออก
+    }
+    
+    if ( result == number ) {//ตรวจสอบว่าเลขที่คิดได้เท่ากับเลขที่inputไปมั้ย
+        return 1 ;//Pass.
+    } else {
+        return 0 ;//Not Pass.
+    }       
+}//end function
+
+int main() {
+
+    int number ;
+
+    printf( "Enter Number: " ) ;
+    scanf( "%d", &number ) ;
+
+    if ( Armstrong( number ) ) {
+        printf( "Pass. \n" ) ;
+    } else {
+        printf( "Not Pass. \n" ) ;
+    }    
+
+    return 0 ;
+}//end main function
