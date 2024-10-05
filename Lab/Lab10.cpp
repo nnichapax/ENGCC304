82b42eb11243c4db03a4eb64a4868105dbbb029d
MOOMIN <nichapha8895@gmail.com>
2024-10-05 13:29:52 +0700

    Update Lab10.cpp

--- Lab/Lab10.cpp
+++ Lab/Lab10.cpp
@@ -1,32 +1,3 @@
-/*
-    จงเขียนโปรแกรมเพื่อรับคำจากผู้ใช้งาน เพื่อตรวจสอบว่า คำที่กรอกมามีลักษณะเป็นคำหรือวลีที่สามารถอ่านจากหลังไปหน้าหรือหน้าไปหลังแล้วยังคงความหมายเหมือนเดิมได้
-    โดยที่ หากคำนั้นสามารถอ่านจากหน้าไปหลังหรือหลังไปได้ ให้แสดงผลลัพธ์ว่า Pass แต่หากทำไม่ได้ให้ขึ้นว่า Not Pass
-
-    Test case:
-        Enter word:
-            radar
-    Output:
-        Pass.
-
-    Test case:
-        Enter word:
-            hello
-    Output:
-        Not Pass.
-
-    Test case:
-        Enter word:
-            Radar
-    Output:
-        Pass.
-
-    Test case:
-        Enter word:
-            here
-    Output:
-        Not Pass.
-*/
-
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>//ใช้นำเข้าหัวข้อไฟล์ (header file) ที่ใช้ในการจัดการกับตัวอักษร
